f5c741466162a62ff9d2e09e6fc5c994d2360cfc
Felix <fh@smile.nl>
2019-05-15 18:53:46 +0200

    fixed gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -8,20 +8,11 @@ var rename = require("gulp-rename");
 var uglify = require('gulp-uglify');
 var pkg = require('./package.json');
 
-// Set the banner content
-var banner = ['/*!\n',
-    ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
-    ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
-    ' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n',
-    ' */\n',
-    ''
-].join('');
-
 // Compile LESS files from /less into /css
 gulp.task('less', function() {
     return gulp.src('less/agency.less')
         .pipe(less())
-        .pipe(header(banner, { pkg: pkg }))
+       // .pipe(header(banner, { pkg: pkg }))
         .pipe(gulp.dest('css'))
         .pipe(browserSync.reload({
             stream: true
@@ -43,7 +34,7 @@ gulp.task('minify-css', ['less'], function() {
 gulp.task('minify-js', function() {
     return gulp.src('js/agency.js')
         .pipe(uglify())
-        .pipe(header(banner, { pkg: pkg }))
+        //.pipe(header(banner, { pkg: pkg }))
         .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('js'))
         .pipe(browserSync.reload({
@@ -97,7 +88,7 @@ gulp.task('dev', ['browserSync', 'less', 'minify-css', 'minify-js'], function() 
 gulp.task('sass', function() {
     return gulp.src('scss/oli.scss')
         .pipe(sass())
-        .pipe(header(banner, { pkg: pkg }))
+        //.pipe(header(banner, { pkg: pkg }))
         .pipe(gulp.dest('css'))
         .pipe(browserSync.reload({
             stream: true
