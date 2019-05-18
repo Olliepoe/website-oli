e3907796d8fcf5a81251c831518721c3c0fa2446
Felix <fh@smile.nl>
2019-05-18 14:27:42 +0200

    cleanup less compiler

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,9 @@
 var gulp = require('gulp');
-var less = require('gulp-less');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
-var header = require('gulp-header');
 var cleanCSS = require('gulp-clean-css');
 var rename = require("gulp-rename");
 var uglify = require('gulp-uglify');
-var pkg = require('./package.json');
-
-// Compile LESS files from /less into /css
-gulp.task('less', function() {
-    return gulp.src('less/agency.less')
-        .pipe(less())
-       // .pipe(header(banner, { pkg: pkg }))
-        .pipe(gulp.dest('css'))
-        .pipe(browserSync.reload({
-            stream: true
-        }))
-});
 
 // Minify compiled CSS
 gulp.task('minify-css', ['sass'], function() {
@@ -27,19 +13,18 @@ gulp.task('minify-css', ['sass'], function() {
         .pipe(gulp.dest('css'))
         .pipe(browserSync.reload({
             stream: true
-        }))
+        }));
 });
 
 // Minify JS
 gulp.task('minify-js', function() {
     return gulp.src('js/agency.js')
         .pipe(uglify())
-        //.pipe(header(banner, { pkg: pkg }))
         .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('js'))
         .pipe(browserSync.reload({
             stream: true
-        }))
+        }));
 });
 
 // Copy vendor libraries from /node_modules into /vendor
@@ -75,7 +60,7 @@ gulp.task('browserSync', function() {
 
 // Dev task with browserSync
 gulp.task('dev', ['browserSync', 'less', 'minify-css', 'minify-js'], function() {
-    gulp.watch('less/*.less', ['less']);
+    gulp.watch('scss/*.scss', ['sass']);
     gulp.watch('css/*.css', ['minify-css']);
     gulp.watch('js/*.js', ['minify-js']);
     // Reloads the browser whenever HTML or JS files change
@@ -88,9 +73,8 @@ gulp.task('dev', ['browserSync', 'less', 'minify-css', 'minify-js'], function() 
 gulp.task('sass', function() {
     return gulp.src('scss/oli.scss')
         .pipe(sass())
-        //.pipe(header(banner, { pkg: pkg }))
         .pipe(gulp.dest('css'))
         .pipe(browserSync.reload({
             stream: true
-        }))
+        }));
 });
