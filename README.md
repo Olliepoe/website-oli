8a5c258cb8811cc749d1da831ca1e1e6951cb86d
Felix <fh@smile.nl>
2019-09-07 14:38:23 +0200

    cleanup

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Website oli
 
+## dev
+pscp C:\Repository\website-oli\robots.txt pi@192.168.178.36:/home/pi/oli
+
+## Developing Using Source Files
+To use the source files, you will need to have npm installed globally along with Gulp.js. To start:
+* Run `npm install` in the root directory
+* Run `gulp dev` and edit the files as needed
+
+If you need to update the plugins included with this template, simply run the following tasks:
+* First run `npm update` to update the dependencies
+* Then run `gulp copy` to copy the new versions to their proper destinations
+
+# Creator
+forked from [Agency](http://startbootstrap.com/template-overviews/agency/)
+
+Start Bootstrap was created by and is maintained by **[David Miller](http://davidmiller.io/)**, Owner of [Blackrock Digital](http://blackrockdigital.io/).
+
+* https://twitter.com/davidmillerskt
+* https://github.com/davidtmiller
--- js/oli.js
+++ js/oli.js
@@ -26,25 +26,7 @@
         offset: 56
     });
 
-    // Collapse Navbar
-    var navbarCollapse = function() {
-        if ($("#mainNav").offset().top > 100) {
-            $("#mainNav").addClass("navbar-shrink");
-        } else {
-            $("#mainNav").removeClass("navbar-shrink");
-        }
-    };
-    // Collapse now if page is not at top
-    navbarCollapse();
-    // Collapse the navbar when page is scrolled
-    $(window).scroll(navbarCollapse);
-
-    // Hide navbar when modals trigger
-    $('.portfolio-modal').on('show.bs.modal', function(e) {
-        $('.navbar').addClass('d-none');
-    })
-    $('.portfolio-modal').on('hidden.bs.modal', function(e) {
-        $('.navbar').removeClass('d-none');
-    })
-
+    $(window).scroll(function() {
+        $("#mainNav").toggleClass('navbar-shrink', $("#mainNav").offset().top > 100);
+    }).scroll();
 })(jQuery); // End of use strict
